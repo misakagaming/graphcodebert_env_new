092120f5a1f0b014549286b96890a4b2d96ae1d1
Eray Erer <eray.erer@hotmail.com>
2024-07-13 09:16:48 +0300

    more tokenizers

--- containers/graphcodebert-env/translation/tokenizer.py
+++ containers/graphcodebert-env/translation/tokenizer.py
@@ -38,10 +38,10 @@ tokenizer.train(files=paths, vocab_size=50265, min_frequency=2,
 #Save the Tokenizer to disk
 tokenizer.save_model(r"C:\Users\eraye\Desktop\graphcodebert-env\containers\graphcodebert-env\translation\tokenizer_segmented")
 
-paths = [str(x) for x in Path(r"C:\Users\eraye\Desktop\graphcodebert-env\containers\graphcodebert-env\gen2java\Training-base\train.coolgen")
+paths = [str(x) for x in Path(r"C:\Users\eraye\Desktop\graphcodebert_env_new\containers\graphcodebert-env\gen2java\Training-base\train.coolgen")
          .glob("*.gensrc")]
 
-for x in Path(r"C:\Users\eraye\Desktop\graphcodebert-env\containers\graphcodebert-env\gen2java\Training\train.java").glob("*.java"):
+for x in Path(r"C:\Users\eraye\Desktop\graphcodebert_env_new\containers\graphcodebert-env\gen2java\Training-base\train.java").glob("*.java"):
     paths.append(str(x))
 
 # Initialize a tokenizer
@@ -58,4 +58,28 @@ tokenizer.train(files=paths, vocab_size=50265, min_frequency=2,
                                 "<mask>",
 ])
 #Save the Tokenizer to disk
-tokenizer.save_model(r"C:\Users\eraye\Desktop\graphcodebert-env\containers\graphcodebert-env\translation\tokenizer_coolgen")
+tokenizer.save_model(r"C:\Users\eraye\Desktop\graphcodebert_env_new\containers\graphcodebert-env\translation\tokenizer_coolgen_base")
+
+
+paths = [str(x) for x in Path(r"C:\Users\eraye\Desktop\graphcodebert_env_new\containers\graphcodebert-env\gen2java\Training2\train.coolgen")
+         .glob("*.gensrc")]
+
+for x in Path(r"C:\Users\eraye\Desktop\graphcodebert_env_new\containers\graphcodebert-env\gen2java\Training2\train.java").glob("*.java"):
+    paths.append(str(x))
+
+# Initialize a tokenizer
+tokenizer = ByteLevelBPETokenizer(lowercase=True)
+
+# Customize training
+tokenizer.train(files=paths, vocab_size=50265, min_frequency=2,
+                show_progress=True,
+                special_tokens=[
+                                "<s>",
+                                "<pad>",
+                                "</s>",
+                                "<unk>",
+                                "<mask>",
+])
+#Save the Tokenizer to disk
+tokenizer.save_model(r"C:\Users\eraye\Desktop\graphcodebert_env_new\containers\graphcodebert-env\translation\tokenizer_coolgen_mod")
+
