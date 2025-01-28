d9f88977c313333213de71336d07b766b9d7d2ea
Eray Erer <eray.erer@hotmail.com>
2025-01-28 15:03:19 +0300

    help

--- containers/graphcodebert-env/translation/codebert_sum/run.py
+++ containers/graphcodebert-env/translation/codebert_sum/run.py
@@ -277,6 +277,8 @@ def main():
         
     config_class, model_class, tokenizer_class = MODEL_CLASSES[args.model_type]
     config = config_class.from_pretrained(args.config_name if args.config_name else args.model_name_or_path)
+    if args.do_lamner:
+        config.hidden_size = 512
     tokenizer = tokenizer_class.from_pretrained(args.tokenizer_name if args.tokenizer_name else args.model_name_or_path,do_lower_case=args.do_lower_case)
     
     #build model
