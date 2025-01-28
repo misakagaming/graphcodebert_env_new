0737ef7bd5d1415398b4cce35ca26bd58a438458
Eray Erer <eray.erer@hotmail.com>
2025-01-28 15:31:54 +0300

    help

--- containers/graphcodebert-env/translation/codebert_sum/run.py
+++ containers/graphcodebert-env/translation/codebert_sum/run.py
@@ -284,7 +284,7 @@ def main():
     tokenizer = tokenizer_class.from_pretrained(args.tokenizer_name if args.tokenizer_name else args.model_name_or_path,do_lower_case=args.do_lower_case)
     
     #build model
-    encoder = model_class.from_pretrained(args.model_name_or_path,config=config)    
+    encoder = model_class.from_pretrained(args.model_name_or_path,config=config, ignore_mismatched_sizes = True)    
     decoder_layer = nn.TransformerDecoderLayer(d_model=config.hidden_size, nhead=config.num_attention_heads)
     decoder = nn.TransformerDecoder(decoder_layer, num_layers=6)
     model=Seq2Seq(encoder=encoder,decoder=decoder,config=config,
